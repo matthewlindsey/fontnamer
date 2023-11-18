8f5259f6a1987e358349573034c3080a2d8c7da5
Matthew Lindsey <mbl@null.net>
2023-11-17 23:53:22 -0700

    Convert script to installable package

--- src/fontnamer/fontnamer.py
+++ src/fontnamer/fontnamer.py
@@ -1,58 +1,46 @@
 #!/usr/bin/env python3
 
-# ==========================================================================
-# fontname.py
-# Copyright 2019 Christopher Simpkins
-# MIT License
-#
-# Dependencies:
-#   1) Python 3.6+ interpreter
-#   2) fonttools Python library (https://github.com/fonttools/fonttools)
-#         - install with `pip3 install fonttools`
-#
-# Usage:
-#   python3 fontname.py [FONT FAMILY NAME] [FONT PATH 1] <FONT PATH ...>
-#
-# Notes:
-#   Use quotes around font family name arguments that include spaces
-# ===========================================================================
-
 import os
 import sys
 
 from fontTools import ttLib
 
+from utils import get_version, file_exists
 
-def main(argv):
+
+def main():
+    argv = sys.argv
+
     # command argument tests
-    print(" ")
-    if len(argv) < 2:
+    if len(argv) == 2 and argv[1] == "-v":
+        sys.stderr.write(f"{get_version()}{os.linesep}")
+        sys.exit(1)
+
+    if len(argv) < 3:
+        sys.stderr.write(f"Error: you did not provide enough arguments.{os.linesep}")
         sys.stderr.write(
-            f"[fontname.py] ERROR: you did not include enough arguments to the script.{os.linesep}"
-        )
-        sys.stderr.write(
-            f"Usage: python3 fontname.py [FONT FAMILY NAME] [FONT PATH 1] <FONT PATH ...>{os.linesep}"
+            f"Usage:{os.linesep}  - fontnamer family_name font_file...{os.linesep}  - fontnamer -v{os.linesep}"
         )
         sys.exit(1)
 
     # begin parsing command line arguments
     try:
-        font_name = str(argv[0])  # the first argument is the new typeface name
+        font_name = str(argv[1])  # the first argument is the new typeface name
     except Exception as e:
         sys.stderr.write(
-            f"[fontname.py] ERROR: Unable to convert argument to string. {e}{os.linesep}"
+            f"Error: unable to convert argument to string. {e}{os.linesep}"
         )
         sys.exit(1)
 
     # all remaining arguments on command line are file paths to fonts
-    font_path_list = argv[1:]
+    font_path_list = argv[2:]
 
     # iterate through all paths provided on command line and rename to `font_name` defined by user
     for font_path in font_path_list:
         # test for existence of font file on requested file path
         if not file_exists(font_path):
             sys.stderr.write(
-                f"[fontname.py] ERROR: the path '{font_path}' does not appear to be a valid file path.{os.linesep}"
+                f"Error: the path '{font_path}' does not appear to be a valid file path.{os.linesep}"
             )
             sys.exit(1)
 
@@ -70,22 +58,19 @@ def main(argv):
         # test that a style name was found in the OpenType tables of the font
         if len(style) == 0:
             sys.stderr.write(
-                f"[fontname.py] Unable to detect the font style from the OpenType name table in '{font_path}'. {os.linesep}"
+                f"Error: unable to detect the font style from the OpenType name table in '{font_path}'. {os.linesep}"
             )
-            sys.stderr.write("Unable to complete execution of the script.")
+            sys.stderr.write("Could not to complete execution of the script.")
             sys.exit(1)
         else:
-            # used for the Postscript name in the name table (no spaces allowed)
-            postscript_font_name = font_name.replace(" ", "")
             # font family name
             nameID1_string = font_name
-            nameID16_string = font_name
             # full font name
             nameID4_string = f"{font_name} {style}"
-            # Postscript name
-            # - no spaces allowed in family name or the PostScript suffix. should be dash delimited
-            nameID6_string = f"{postscript_font_name}-{style.replace(' ', '')}"
-            # nameID6_string = postscript_font_name + "-" + style.replace(" ", "")
+            # postscript name - no spaces allowed, should be dash delimited
+            nameID6_string = f"{font_name.replace(" ", "")}-{style.replace(' ', '')}"
+            # typographic family name
+            nameID16_string = font_name
 
             # modify the opentype table data in memory with updated values
             for record in namerecord_list:
@@ -107,28 +92,18 @@ def main(argv):
                     cff.cff.fontNames = [nameID6_string]
                 except Exception as e:
                     sys.stderr.write(
-                        f"[fontname.py] ERROR: unable to write new names to CFF table: {e}"
+                        f"Error: unable to write new names to CFF table: {e}"
                     )
 
         # write changes to the font file
         try:
             tt.save(font_path)
-            print(f"[OK] Updated '{font_path}' with the name '{nameID4_string}'")
+            print(
+                f"Successfully updated '{font_path}' with the name '{nameID4_string}'"
+            )
         except Exception as e:
             sys.stderr.write(
-                f"[fontname.py] ERROR: unable to write new name to OpenType name table for '{font_path}'. {os.linesep}"
+                f"Error: unable to write new name to OpenType name table for '{font_path}'. {os.linesep}"
             )
             sys.stderr.write(f"{e}{os.linesep}")
             sys.exit(1)
-
-
-# Utilities
-
-
-def file_exists(filepath):
-    """Tests for existence of a file on the string filepath"""
-    return os.path.exists(filepath) and os.path.isfile(filepath)
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
---
+++ src/fontnamer/utils.py
@@ -0,0 +1,42 @@
+import os
+
+def get_version():
+    # use auto-generated version file managed by setuptool_scm
+    def use_version_file():
+        try:
+            from _version import __version__
+
+            return __version__
+        except ImportError:
+            return False
+
+    # use setuptool_scm library
+    def use_setuptool_scm():
+        try:
+            from setuptools_scm import get_version
+
+            return get_version(
+                version_scheme="no-guess-dev", local_scheme="no-local-version"
+            )
+        except ImportError:
+            return False
+
+    # try reading package metadata using importlib
+    def use_package_metadata():
+        try:
+            from importlib.metadata import version, PackageNotFoundError
+
+            return version("fontnamer")
+        except PackageNotFoundError:
+            return False
+
+    return (
+        use_version_file() or use_setuptool_scm() or use_package_metadata() or "unknown"
+    )
+
+
+
+def file_exists(filepath):
+    """Tests for existence of a file on the string filepath"""
+    return os.path.exists(filepath) and os.path.isfile(filepath)
+
